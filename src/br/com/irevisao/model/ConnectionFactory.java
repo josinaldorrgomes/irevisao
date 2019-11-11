96439a0a904cd78aea263f4ad7c45c277e607922
Josinaldo Gomes <josinaldorobertorocha@gmail.com>
2019-11-11 17:37:03 -0300

    Update ConnectionFactory.java

--- src/br/com/irevisao/model/ConnectionFactory.java
+++ src/br/com/irevisao/model/ConnectionFactory.java
@@ -14,15 +14,6 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
 	public Connection getConnection() {
-		/**
-		 * Criando uma conexao com o banco de dados atraves do driver JDBC. O bloco de
-		 * codigo try faz o a conexao com o banco de dados irevisao_db no hospedeiro
-		 * local, passando o email e a senha.
-		 * IP: 192.175.124.120
-		 * BD: irevisao_bd
-		 * User: irevisao_user
-		 * Password: /;@8ZNz9D5B
-		 */
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/irevisao_db", "root", "1123581f");
